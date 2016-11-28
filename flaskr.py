21cdae536cb0155e70df3bc8ed641cbf9553a01f
SakuraCase <sakurada6617@gmail.com>
2016-11-28 19:46:43 +0900

    Step4

--- flaskr.py
+++ flaskr.py
@@ -15,13 +15,17 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
-
 
 def get_db():
     """Opens a new database connection if there is none yet for the 
@@ -31,13 +35,13 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-
 if __name__ == "__main__":
+    with app.app_context():
+        init_db()
     app.run(host="0.0.0.0", debug=True)
